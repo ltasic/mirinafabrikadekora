a2cb13253eff8029436f448f401f683fa35364bf
Luka Tasic <lukatasic@Lukas-MacBook-Pro.local>
2021-07-05 12:41:45 +0200

    Toggler and font reduced

--- site/src/app/about/about.component.ts
+++ site/src/app/about/about.component.ts
@@ -1,7 +1,4 @@
-import { AotSummaryResolver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ImageServiceService } from '../services/image-service.service';
-import * as AOS from 'aos';
 
 @Component({
   selector: 'app-about',
@@ -9,60 +6,9 @@ import * as AOS from 'aos';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  public slideIndex: number = 1;
-  public slides: { img: string, id: number }[];
 
-  constructor(private imageService: ImageServiceService) {
-    this.slides = this.imageService.getImagesByCategory(6);
-  }
-
-  public plusSlides(n: number) {
-    this.showSlides(this.slideIndex += n);
-  }
-
-  public currentSlide(n: number) {
-    this.showSlides(this.slideIndex = n);
-  }
-
-  public getSlideIndex() {
-    return this.slideIndex;
-  }
-
-  public showSlides(n: number) {
-    if (n > this.slides.length) {
-      this.slideIndex = 1
-    }
-    if (n < 1) {
-      this.slideIndex = this.slides.length
-    }
-  }
-
-  public showImage(index: number) {
-    let displayChoice: string;
-    let slideIndex = this.getSlideIndex();
-
-    if (index === slideIndex) {
-      displayChoice = "block";
-    }
-    else {
-      displayChoice = "none";
-    }
-
-    return {
-      display: displayChoice
-    }
-  }
-
-  public setActive(index: number) {
-    let slideIndex = this.getSlideIndex();
-
-    return {
-      "active": index === slideIndex,
-      "": index !== slideIndex
-    };
-  }
-
+  constructor() {}
+  
   ngOnInit(): void {
-    AOS.init();
   }
 }
